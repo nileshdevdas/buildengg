e0647b9ec7d8874fda5e3d0f13763210d030b906
nileshdevdas <nilesh.devdas@gmail.com>
2019-02-05 07:52:02 +0530

    Intial Commit

---
+++ src/test/java/com/HelloWorldTest.java
@@ -0,0 +1,27 @@
+package com;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+
+
+    @Test
+    public void printName() {
+
+    }
+
+    @Test
+    public void printName1() {
+
+    }
+
+    @Test
+    public void printName2() {
+
+    }
+
+}
